2c4feff26650732e8e06e778c20400cebeafb4f6
Jens Krause <jens@rasta.online>
2020-05-11 14:32:44 +0700

    customize colors and text

--- app/src/main/java/com/example/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/customfancontroller/DialView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
@@ -46,8 +47,18 @@ class DialView @JvmOverloads constructor(
         typeface = Typeface.create("", Typeface.BOLD)
     }
 
+    private var fanSpeedLowColor = 0
+    private var fanSpeedMediumColor = 0
+    private var fanSpeedMaxColor = 0
+
     init {
         isClickable = true
+
+        context.withStyledAttributes(attrs, R.styleable.DialView) {
+            fanSpeedLowColor = getColor(R.styleable.DialView_fanColor1,0)
+            fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2, 0)
+            fanSpeedMaxColor = getColor(R.styleable.DialView_fanColor3,0)
+        }
     }
     // implement click action
     override fun performClick(): Boolean {
@@ -78,7 +89,12 @@ class DialView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         // set color depending if fan is on or off
-        paint.color = if(fanSpeed==FanSpeed.OFF) Color.GRAY else Color.GREEN
+        paint.color = when(fanSpeed){
+            FanSpeed.OFF ->Color.GRAY
+            FanSpeed.LOW -> fanSpeedLowColor
+            FanSpeed.MEDIUM -> fanSpeedMediumColor
+            FanSpeed.HIGH -> fanSpeedMaxColor
+        }
         // draw circle for dial
         canvas.drawCircle((width/2).toFloat(), (height/2).toFloat(),radius,paint)
 
